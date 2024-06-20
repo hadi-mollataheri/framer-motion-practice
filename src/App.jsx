4a45faa516617cdbaf8930e27195c1f1e92eb72e
Hadi <mhadimollataheri@gmail.com>
2024-06-20 11:11:58 +0330

    Use animation when component unmounts

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
+import { useState } from 'react';
 import './App.css';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
 function App() {
+  const [isVisible, setIsVisible] = useState(true);
+
   return (
     <div>
-      <button className='example-button'>Show/Hide</button>
-      <motion.div
-        initial={{ rotate: '0deg' }}
-        animate={{ rotate: '180deg' }}
-        transition={{
-          duration: 2
-        }}
-        style={{
-          width: 150,
-          height: 150,
-          background: 'black',
-          margin: 10,
-        }}
-      ></motion.div>
+      <button
+        onClick={() => setIsVisible(!isVisible)}
+        className='example-button'
+      >
+        Show/Hide
+      </button>
+      <AnimatePresence>
+        {isVisible && (
+          <motion.div
+            initial={{ rotate: '0deg' }}
+            animate={{ rotate: '180deg' }}
+            transition={{
+              duration: 2,
+              ease: 'backInOut',
+            }}
+            exit={{ rotate: '0deg' }}
+            style={{
+              width: 150,
+              height: 150,
+              background: 'black',
+              margin: 10,
+            }}
+          ></motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
