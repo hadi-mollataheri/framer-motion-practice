f12e073c45542c1c269f6ce12ddc7d00f1d11ba5
Hadi <mhadimollataheri@gmail.com>
2024-06-24 17:29:41 +0330

    Use whileInView

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import Basic from './components/Basic';
 import Gestures from './components/Gestures';
 import AnimationControls from './components/AnimationControls';
 import './App.css';
+import ViewBasedAnimations from './components/ViewBasedAnimations';
+
 function App() {
   return (
     <div className='container'>
       <Basic />
       <Gestures />
       <AnimationControls />
+      <ViewBasedAnimations />
     </div>
   );
 }
---
+++ src/components/ViewBasedAnimations.jsx
@@ -0,0 +1,17 @@
+import { motion } from 'framer-motion';
+
+const ViewBasedAnimations = () => {
+  return (
+    <>
+      <div style={{ height: '101vh' }} />
+      <motion.div
+        initial={{ opacity: 0, scale: 0.6 }}
+        whileInView={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1.2 }}
+        style={{ height: '1000px', width: '100%', backgroundColor: 'green' }}
+      />
+    </>
+  );
+};
+
+export default ViewBasedAnimations;
